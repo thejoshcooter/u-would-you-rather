873168d07634e077bc0797171f9d9a9403baae5f
Josh <thejoshcooter@gmail.com>
2021-04-26 05:42:34 -0500

    add basic structure for menu and setup routing

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,12 +1,62 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const MainMenu = () => {
     return (
         <>
-            main menu
+        <Menu>
+            <Container>
+                <Logo>Would You Rather?</Logo>
+
+                <Nav>
+                    <Link to='/'><button>Authentication</button></Link>
+                    <Link to='/dashboard'><button>Dashboard</button></Link>
+                    <Link to='/leaderboard'><button>Leaderboard</button></Link>
+                    <Link to='/create'><button>Create Question</button></Link>
+                </Nav>
+
+                <Account>
+                    <h3>User</h3>
+                    <button>Logout</button>
+                </Account>
+            </Container>
+        </Menu>
         </>
     )
 }
+
+const Menu = styled.div`
+    width: 100%;
+    height: 3rem;
+    border: 1px solid black;
+`
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 1400px;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+`
+
+const Logo = styled.div`
+
+`
+
+const Nav = styled.div`
+
+`
+
+const Account = styled.div`
+    margin-left: auto;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    align-items: center;
+`
 
 export default MainMenu
