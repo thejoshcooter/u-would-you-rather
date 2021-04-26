5311fb7096598dce2499774d564baae54ee7f8d8
Josh <thejoshcooter@gmail.com>
2021-04-26 07:27:12 -0500

    lay out base structure for dashboard

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const DashboardView = () => {
     return (
         <>
-            dashboard view
+        <Container>
+            <Tabs>
+                <div>Unanswered</div>
+                <div>Answered</div>
+            </Tabs>
+        </Container>
         </>
     )
 }
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 1400px;
+    height: 800px;
+    margin: 0 auto;
+    margin-top: 3rem;
+    border: 1px solid black;
+`
+
+const Tabs = styled.div`
+    height: 3rem;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid black;
+
+    div {
+        width: 10rem;
+        height: 100%;
+        margin: 0 1rem;
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid black;
+    }
+`
 
 export default DashboardView
--- src/views/leaderboard/index.js
+++ src/views/leaderboard/index.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const LeaderboardView = () => {
     return (
         <>
-            leaderboard view
+            <Container>
+                <Leaderboard>
+                    <h2>Leaderboard</h2>
+                </Leaderboard>
+            </Container>
         </>
     )
 }
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 1400px;
+    height: auto;
+    margin: 0 auto;
+    margin-top: 3rem;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+`
+
+const Leaderboard = styled.div`
+    width: 60%;
+    height: 50rem;
+    border: 1px solid black;
+`
 
 export default LeaderboardView
