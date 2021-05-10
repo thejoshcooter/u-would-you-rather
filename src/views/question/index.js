3c330a46f648818c93b8d3c7d0d5c9105e48f287
thejoshcooter <thejoshcooter@gmail.com>
2021-05-10 06:01:49 -0500

    fix 404 functionality

--- src/views/question/index.js
+++ src/views/question/index.js
@@ -4,12 +4,14 @@ import { useSelector } from 'react-redux'
 import { useRouteMatch, Redirect } from 'react-router-dom'
 
 import QuestionCard from '../../components/QuestionCard'
+import Error404 from '../404/index'
 
 const QuestionView = () => {
     const questions = useSelector(state => state.questions.data)
     const answers = useSelector(state => state.auth.answers)
     const questionId = useRouteMatch().params.id
     const questionExists = questions.some(question => question.id === questionId)
+    console.log('questions: ', questions, 'question exists: ', questionExists)
 
     return (
         <>
@@ -30,8 +32,12 @@ const QuestionView = () => {
                     }
                 })}
 
-                {!questionExists && (
-                    <Redirect to='/404' />
+                {questions.length < 1 &&  (
+                    <p>loading...</p>
+                )}
+
+                {questions.length > 1 && !questionExists && (
+                    <Error404 />
                 )}
             </Container>
         </>
